1120ca003b254e82c85d035cde3e133c98ded3f9
Burkhon <burkhon85@gmail.com>
2022-10-13 21:09:27 -0400

    updated config file and ajusted test

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -12,45 +12,32 @@ public class HomeTest extends BaseTest {
     HomePage page;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         page = new HomePage(driver);
     }
-
-    @Test(testName = "Google title test")
-    public void test01(){
-        page.assertEquals(driver.getTitle(), "Google");
-    }
-
-    @Test(testName = "Google title test")
-    public void burkhon(){
-        System.out.println("Burkhon");
-    }
-
 
     @Test(testName = "US 306 - Filter options")
     public void US306() {
 
     }
 
-        @Test(testName = "US 301 - verify standard user can log in with right password")
-        public void testErik01 () {
+    @Test(testName = "US 301 - verify standard user can log in with right password")
+    public void testErik01() {
 
-            driver.findElement(By.id("user-name")).sendKeys("standard_user");
-            driver.findElement(By.id("password")).sendKeys("secret_sauce");
-            driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
 
 
-        }
+    }
 
+
+    @Test(testName = "US 302 - error message = Epic sadface: Sorry, this user has been locked out displays")
+    public void testErik02() {
+
+        String expected = "Epic sadface: Sorry, this user has been locked out displays";
+        String actual = driver.findElement(By.xpath("//h3")).getText();
+        Assert.assertEquals(expected, actual);
+    }
 }
-//
-//    @Test(testName = "US 302 - error message = Epic sadface: Sorry, this user has been locked out displays")
-//    public void test02() {
-//
-//        String expected = "Epic sadface: Sorry, this user has been locked out displays";
-//        String actual = driver.findElement(By.xpath("//h3")).getText();
-//        Assert.assertEquals(expected, actual);
-//
-//        @Test(testName = "US 303 - ")
-
 
