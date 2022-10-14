cd023c723bc0ad6bc999f6f713c40aae0ffa4e0a
Oksana Skrynnik <oksanaskrynnik@oksanas-mbp.mynetworksettings.com>
2022-10-13 20:19:54 -0400

    footer Naming added

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import base.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,11 +9,14 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
 
-    public HomePage(WebDriver driver){
+    public HomePage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
 
     @FindBy(id = "sample")
     public WebElement sampleElement;
+
+    @FindBy(xpath = "//div[@class=\"footer_copy\"]")
+    public WebElement footerNaming;
 }
