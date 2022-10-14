ddc77bff06d4cf00b4525892798f0c08b9c735f3
D Alexandra <alexandra.davlitkireeva@gmail.com>
2022-10-13 20:39:53 -0400

    added 3 test cases

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -15,4 +15,35 @@ public class HomePage extends BasePage {
 
     @FindBy(id = "sample")
     public WebElement sampleElement;
+
+    @FindBy(xpath = "//a[@class='shopping_cart_link']")
+    public WebElement shoppingCartBtn;
+
+    @FindBy(id = "checkout")
+    public WebElement checkoutBtn;
+
+    @FindBy(id = "first-name")
+    public WebElement firstNameField;
+
+    @FindBy(id = "lastName")
+    public WebElement lastNameField;
+
+    @FindBy(id = "postal-code")
+    public WebElement zipField;
+
+//    @FindBy(id = "cancel")
+//    public WebElement cancelBtn;
+//
+//    @FindBy(id = "continue-shopping")
+//    public WebElement contShopBtn;
+
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    public WebElement addToCart;
+
+    @FindBy(xpath = "//div[text()='Sauce Labs Backpack']")
+    public WebElement itemInCart;
+
+    @FindBy(id = "remove-sauce-labs-backpack")
+    public WebElement removeBtn;
+
 }
--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,5 +27,28 @@ public class HomeTest extends BaseTest {
         System.out.println("Burkhon");
     }
 
+    @Test(testName = "US308 - Checkout:Your Information")
+    public void testAlex01() {
+
+//        Alert alert = driver.switchTo().alert();
+//        alert.accept();
+        page.shoppingCartBtn.click();
+        page.checkoutBtn.click();
+        page.firstNameField.isDisplayed();
+        page.lastNameField.isDisplayed();
+        page.zipField.isDisplayed();
+    }
+    @Test(testName = "US 309 - Add to cart")
+    public void testAlex02() {
+    page.addToCart.click();
+    page.shoppingCartBtn.click();
+    Assert.assertEquals(page.itemInCart.getText(), "Sauce Labs Backpack");
+    }
+    @Test(testName = "US 310 - Remove from cart")
+    public void testAlex03() {
+        page.addToCart.click();
+        page.shoppingCartBtn.click();
+        page.removeBtn.click();
+    }
 
 }
