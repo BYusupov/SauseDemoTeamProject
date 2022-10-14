864070f1d853284564eba4fa97c117c8e1f74a39
MurilloLab <emurillo1800@gmail.com>
2022-10-13 23:28:56 -0400

    A

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -30,8 +30,8 @@ public class HomeTest extends BaseTest {
     @Test(testName = "US 301 - verify standard user can log in with right password")
     public void testErik01() {
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("user-name")).sendKeys("username");
+        driver.findElement(By.id("password")).sendKeys("password");
         driver.findElement(By.id("login-button")).click();
 
     }
