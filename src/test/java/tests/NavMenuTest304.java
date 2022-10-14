e1359f9a49a6d54763fc44a3ec046686212c75b9
Burkhon <burkhon85@gmail.com>
2022-10-14 15:34:12 -0400

    fexided few things

--- src/test/java/tests/NavMenuTest304.java
+++ src/test/java/tests/NavMenuTest304.java
@@ -4,6 +4,7 @@ import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.HomePage;
 import pages.NavMenuPage304;
 import utils.ConfigReader;
 
@@ -11,17 +12,18 @@ import utils.ConfigReader;
 public class NavMenuTest304 extends BaseTest {
 
     private NavMenuPage304 page;
-
+    HomePage homePage;
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
         page = new NavMenuPage304(driver);
-        page.usernameInput.sendKeys(ConfigReader.readProperty("username"));
-        page.passwordInput.sendKeys(ConfigReader.readProperty("password"));
-        page.loginBtn.click();
+        homePage = new HomePage(driver);
+
     }
 
     @Test(testName = "Navigation menu")
     public void test304() {
+
+        homePage.testErik01();
         // When user clicks the button it should display following buttons:
         page.navBtn.click();
 
--- src/test/java/tests/SocialMediaTest.java
+++ src/test/java/tests/SocialMediaTest.java
@@ -5,21 +5,25 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.HomePage;
 import pages.SocialMediaPage;
 import java.util.ArrayList;
 import java.util.Set;
 
 public class SocialMediaTest extends BaseTest {
     private SocialMediaPage page;
+    HomePage homePage;
 
     @BeforeMethod(alwaysRun = true)
     public void setUp(){
-        driver.findElement(By.linkText("Saucedemo")).click();
+        homePage = new HomePage(driver);
 
         page = new SocialMediaPage(driver);
     }
     @Test(testName = "US 307 - Verify social media buttons present twitter, facebook and linkedIn")
     public void test307(){
+
+        homePage.testErik01();
 
         // click on the icons
         page.click(page.twitterbtn);
