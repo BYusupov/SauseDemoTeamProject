a6211dd4eff1edac9b42871ecf8ac14a14cdc2bf
Oksana Skrynnik <oksanaskrynnik@oksanas-mbp.mynetworksettings.com>
2022-10-13 20:21:13 -0400

    created US305

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,19 +12,26 @@ public class HomeTest extends BaseTest {
     HomePage page;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         page = new HomePage(driver);
     }
 
     @Test(testName = "Google title test")
-    public void test01(){
+    public void test01() {
         page.assertEquals(driver.getTitle(), "Google");
     }
 
     @Test(testName = "Google title test")
-    public void burkhon(){
+    public void burkhon() {
         System.out.println("Burkhon");
     }
 
 
+    @Test(testName = "US305 - Footer of the page should be © 2022 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy")
+    public void testOksana() {
+        String expected = "US305 - Footer of the page should be © 2022 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy";
+        String actual = page.getText(page.footerNaming);
+        Assert.assertEquals(expected, actual);
+
+    }
 }
