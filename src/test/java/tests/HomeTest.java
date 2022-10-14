4aa42a5720633fac41ca82cf1b997f5ef4687f6b
MurilloLab <emurillo1800@gmail.com>
2022-10-13 20:06:57 -0400

    test case 301

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,5 +26,24 @@ public class HomeTest extends BaseTest {
         System.out.println("Burkhon");
     }
 
+    @Test(testName = "US 301 - verify standard user can log in with right password")
+    public void test01() {
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+    }
 
 }
+//
+//    @Test(testName = "US 302 - error message = Epic sadface: Sorry, this user has been locked out displays")
+//    public void test02() {
+//
+//        String expected = "Epic sadface: Sorry, this user has been locked out displays";
+//        String actual = driver.findElement(By.xpath("//h3")).getText();
+//        Assert.assertEquals(expected, actual);
+//
+//        @Test(testName = "US 303 - ")
+
+
