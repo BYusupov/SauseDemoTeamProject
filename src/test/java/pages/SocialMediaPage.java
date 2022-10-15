6a85c6ab74afc09379147f8b6bdb594e7b2f97ff
D Alexandra <alexandra.davlitkireeva@gmail.com>
2022-10-14 20:07:50 -0400

    added anther version for SocialMediaTest

--- src/test/java/pages/SocialMediaPage.java
+++ src/test/java/pages/SocialMediaPage.java
@@ -6,11 +6,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class SocialMediaPage extends BasePage {
     public SocialMediaPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
         }
+
+//        @FindBy(xpath = "ul[@class='social']")
+//        public List<WebElement> mediaIcons;
         @FindBy(xpath = "//li[@class= 'social_twitter']")
         public WebElement twitterbtn;
 
--- src/test/java/tests/SocialMediaTest.java
+++ src/test/java/tests/SocialMediaTest.java
@@ -2,10 +2,12 @@ package tests;
 
 import base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.SocialMediaPage;
+
 import java.util.ArrayList;
 import java.util.Set;
 
@@ -13,20 +15,27 @@ public class SocialMediaTest extends BaseTest {
     private SocialMediaPage page;
 
     @BeforeMethod(alwaysRun = true)
-    public void setUp(){
+    public void setUp() {
         driver.findElement(By.linkText("Saucedemo")).click();
 
         page = new SocialMediaPage(driver);
     }
-    @Test(testName = "US 307 - Verify social media buttons present twitter, facebook and linkedIn")
-    public void test307(){
 
-        // click on the icons
+    @Test(testName = "US 307 - Verify social media buttons present twitter, facebook and linkedIn")
+    public void test307() {
+
+        // another version
+//        for (WebElement eachMedia : page.mediaIcons) {
+//            eachMedia.getText();
+//            Assert.assertTrue(eachMedia.isDisplayed());
+
+
+            // click on the icons
         page.click(page.twitterbtn);
         page.click(page.facebookbtn);
         page.click(page.linkedinbtn);
 
-        //switching to each window
+            //switching to each window
         String mainWindowID = driver.getWindowHandle();
         ArrayList<String> tab = new ArrayList<String>(driver.getWindowHandles());
         Set<String> allWindowIDs = driver.getWindowHandles();
